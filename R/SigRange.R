39850239aa0cdab09e204c58afdc2e89b04f686d
takahirosuzuki1980 <takahirosuzuki1980@gmail.com>
2018-03-28 12:52:15 +0900

    Create SigRange.R
    first commit

---
+++ R/SigRange.R
@@ -0,0 +1,22 @@
+SigRange <- function(motif_counts_matrix = motif_counts_matrix, cutoff = 0.00001, windowSize = 100){
+　　　sigClass <- names(which(motif_counts_matrix[,4] < as.numeric(cutoff))) ##significant Class (set p-value)
+     posiClass <- names(which((motif_counts_matrix[,1] - motif_counts_matrix[,2]) >= 0)) ##to extract only "enriched", select only the class of target is more than ctrl
+     sigPosiClass <- sigClass[sigClass %in% posiClass] ##significant and posi Classes
+     signum <- 1
+     sigrangeStr <- NULL
+     sigrangeEnd <- NULL
+     significant_ranges <- NULL
+     while(length(sigPosiClass) >= signum){
+     　　　sigrangeStr <- c(sigrangeStr,as.numeric(sigPosiClass[signum])-(windowSize/2))
+      	　if(!is.na(sigPosiClass[(signum+1)])){ # if the next class is also "significant", combine
+      		　　　while(abs((as.numeric(sigPosiClass[signum]) - as.numeric(sigPosiClass[(signum+1)]))) <= windowSize){ #If next significant class is less than window size, combine as one region
+      			　　signum <- signum + 1
+      			　　if (signum == length(sigPosiClass)) break
+      		　　　}
+      　　　}
+      　　　sigrangeEnd <- c(sigrangeEnd,as.numeric(sigPosiClass[signum])+(windowSize/2))
+      　　　signum <- signum + 1
+      }
+      significant_ranges <- cbind(sigrangeStr, sigrangeEnd) #Significantly enriched regions
+      return(significant_ranges)
+}
