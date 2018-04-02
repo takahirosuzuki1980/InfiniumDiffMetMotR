b4b7f075b91dedc294ff62f9e0aff844ffc5f306
takahirosuzuki1980 <takahirosuzuki1980@gmail.com>
2018-04-03 07:18:55 +0900

    Update MotScr.R
    final completion
    
    now this is ver1.0

--- R/MotScr.R
+++ R/MotScr.R
@@ -1,15 +1,15 @@
 MotScr <- function(infile="sel_processed_Mval.txt", motifDBList = motifDBList, outname="screening_result", ControlColnum, TreatmentColnum, MethylDemethyl="Demethyl", version = "450"){
 	#This function is a pipline to analyze enrichment of given motif PWMs in differentially methylated probes of illumina arrays.
-	
-	cat("Reading data......\n")
+
+	cat("Reading data...\n")
 	selDataMatrix <- read.table (infile)
 
-	cat("DMP identification......\n")  
+	cat("DMP identification...\n")  
 	#extraction demethylated probes  
 	if((MethylDemethyl == "Demethyl") ||( MethylDemethyl == "D")) {
-	 diff_table <- which((selDataMatrix[,ControlColnum]-selDataMatrix[,TreatmentColnum]) >=2)
+		diff_table <- which((selDataMatrix[,ControlColnum]-selDataMatrix[,TreatmentColnum]) >=2)
 	}else if ((MethylDemethyl == "Methyl" )|| (MethylDemethyl == "M")){
-	 diff_table <- which((selDataMatrix[,ControlColnum]-selDataMatrix[,TreatmentColnum]) <=-2) 
+		diff_table <- which((selDataMatrix[,ControlColnum]-selDataMatrix[,TreatmentColnum]) <=-2) 
 	}
 	DMP_IDs <- rownames(selDataMatrix )[diff_table]
 	nDMP_IDs <- length(DMP_IDs)
@@ -27,21 +27,18 @@ MotScr <- function(infile="sel_processed_Mval.txt", motifDBList = motifDBList, o
 		positionsList <- list("target" = target_position, "random" = random_position)	#integrate DMP positoins and NC positions
 	}
 
+	## sequence extraction
 	##Read human hg19 genomic sequence
 	library("BSgenome.Hsapiens.UCSC.hg19")
 	tmp <- ls(paste("package", "BSgenome.Hsapiens.UCSC.hg19", sep=":"))
 	genome <- eval(parse(text=tmp))
-
-	## sequence extraction
-	cat("Retreave the sequences......\n")
-
+	cat("Retreave the sequences...\n")
 	seq_range <- c(-5000, 5000) #range from the CpG position to be extracted
 	sequences <- lapply(positionsList , function(x){seqExtract(positions = x, genome = genome, seq_range)})
 
 	##make a templrary outpput directory
 	tempDir <- paste(outname, "_temp", sep="")
 	dir.create(tempDir)
-
 	##writing the sequences to splitted files
 	seqs <- sequences$target
 	target_all_filenames <- writeSplitSeq (seqs=seqs, split_num = 2500, tempDir=tempDir, output_file="target" )
@@ -51,67 +48,67 @@ MotScr <- function(infile="sel_processed_Mval.txt", motifDBList = motifDBList, o
 	rm(seqs)
 	invisible(replicate(3, gc()))
 
-	cat("motif search...,,,\n")
+	cat("motif search...\n")
 	## ((multi-fasta file(multi-seqs) x motif) x [motif number])) x [multi-fasta file number]
 	target_positionsList <- splitSeqMotDist(filenames=target_all_filenames,  motif_list=motifDBList)
-	ntarget_hits <- lapply(target_positionsList, function(x){
-		length(unlist(x))}
-		)
+	ntarget_hits <- lapply(target_positionsList, function(x){length(unlist(x))})
 	file.remove(target_all_filenames)
 
 	random_positionsList <- splitSeqMotDist(filenames=random_all_filenames,  motif_list=motifDBList)
-	nrandom_hits <- lapply(random_positionsList, function(x){
-		length(unlist(x))}
-		)
-	 file.remove(random_all_filenames)
-	 file.remove(tempDir)
-	 gc()
+	nrandom_hits <- lapply(random_positionsList, function(x){length(unlist(x))})
+	file.remove(random_all_filenames)
+	file.remove(tempDir)
+	gc()
 
-	 distPlotFile <- paste(Sys.Date(),'_',outname,'_mot_plot.pdf', sep="") ##output file name setting
+	##Plot output setting
+	distPlotFile <- paste(outname,'_plot.pdf', sep="") ##output file name setting
 	pdf(distPlotFile)
-
 	parameter_matrix <- NULL
 	All_motif_names <- names(motifDBList)
+
 	##Computation by each motif in motifDB
 	for (j in 1:length(target_positionsList)){
-	  indicator_2 <- paste("               ", "MOTIF   ",All_motif_names[j],":",j, "/", length(All_motif_names),"      ", date(), "\n", sep="") #monitering of progress (standerd output of vidualizing motif name)
-	  cat(indicator_2)
+		##monitering of progress (standerd output of vidualizing motif name)
+		indicator_2 <- paste("               ", "MOTIF   ",All_motif_names[j],": ",j, "/", length(All_motif_names),"      ", date(), "\n", sep="") 
+		cat(indicator_2)
 
-	  motif_name <- All_motif_names[j]
-	  target_mot_posi <- unlist(target_positionsList[[j]])
-	  ctrl_mot_posi <- unlist(random_positionsList[[j]])
+		motif_name <- All_motif_names[j]
+		target_mot_posi <- unlist(target_positionsList[[j]])
+		ctrl_mot_posi <- unlist(random_positionsList[[j]])
 
-	  ## histogram plot
-	  cat("Step 1 / Plot Histgram (Figure 1)......\n")
-	  motHist(target_mot_posi, ctrl_mot_posi, seq_range=seq_range, motif_name=motif_name)
-	  ## motif enrichment score plot
+		## histogram plot
+		cat("Step 1 / Plot Histgram (Figure 1)...\n")
+		motHist(target_mot_posi, ctrl_mot_posi, seq_range=seq_range, motif_name=motif_name)
+		## motif enrichment score plot
 
-	  cat("Step 2 / Plot Enrichment Score (Figure 2)......\n")
-	  enrichment_scores <- enrichScoreDist(target_mot_posi, ctrl_mot_posi, seq_range=seq_range, motif_name=motif_name, nDMP_IDs=nDMP_IDs, plot_draw=TRUE)
+		cat("Step 2 / Plot Enrichment Score (Figure 2)...\n")
+		enrichment_scores <- enrichScoreDist(target_mot_posi, ctrl_mot_posi, seq_range=seq_range, motif_name=motif_name, nDMP_IDs=nDMP_IDs, plot_draw=TRUE)
 
-	  cat("Step 3 / Creating a count.pvalue Matrix ......\n")
-	  windowSize <- 100
-	  motif_counts_matrix <- countMatrix(target_mot_posi = target_mot_posi, ctrl_mot_posi = ctrl_mot_posi, seq_range=seq_range, windowSize = windowSize, slide = 50)
+		cat("Step 3 / Creating a count.pvalue Matrix...\n")
+		windowSize <- 100
+		motif_counts_matrix <- countMatrix(target_mot_posi = target_mot_posi, ctrl_mot_posi = ctrl_mot_posi, seq_range=seq_range, windowSize = windowSize, slide = 50)
 
-	  if((length(target_mot_posi != 0)) && (length(ctrl_mot_posi != 0))){
-	     cat("Step 4 / Visualization of FC (Figure 3)......\n")
-	     FCChangePlot(motif_name = motif_name, motif_counts_matrix = motif_counts_matrix)
+		if((length(target_mot_posi != 0)) && (length(ctrl_mot_posi != 0))){
+			cat("Step 4 / Visualization of FC (Figure 3)...\n")
+			FCChangePlot(motif_name = motif_name, motif_counts_matrix = motif_counts_matrix)
 
-	     cat("Step 5 / Poisson disribution model exact test (Figure 4)......\n")
-	     PoisTestPlot(motif_counts_matrix = motif_counts_matrix, motif_name = motif_name, seq_range = seq_range)
-	  }
-	 
-	  cat("Step 6 / Extraction of significantly enriched ranges......\n")
-	  significant_ranges <- SigRange(motif_counts_matrix = motif_counts_matrix, cutoff = 0.00001, windowSize = windowSize)
-	  cat("Step 7 / Significance test......\n")
+			cat("Step 5 / Poisson disribution model exact test (Figure 4)...\n")
+			PoisTestPlot(motif_counts_matrix = motif_counts_matrix, motif_name = motif_name, seq_range = seq_range)
+		}
 
-	  parameters <- enrichTest(significant_ranges = significant_ranges, motif_counts_matrix = motif_counts_matrix, seq_range=seq_range, enrichment_scores=enrichment_scores,windowSize=windowSize)
-	  parameter_matrix <- rbind(parameter_matrix, parameters)
+		cat("Step 6 / Extraction of significantly enriched ranges...\n")
+		significant_ranges <- SigRange(motif_counts_matrix = motif_counts_matrix, cutoff = 0.00001, windowSize = windowSize)
+
+		cat("Step 7 / Significance test...\n")
+		parameters <- enrichTest(significant_ranges = significant_ranges, motif_counts_matrix = motif_counts_matrix, seq_range=seq_range, enrichment_scores=enrichment_scores,windowSize=windowSize)
+		parameter_matrix <- rbind(parameter_matrix, parameters)
 	}
 	dev.off()
 
+	cat("Result table writing...\n")
 	finOut <- cbind(motGene, ntarget_hits, nrandom_hits, parameter_matrix)
 	##out put file name setting
 	Mad3ResultOut <- paste(Sys.Date(),'_',outname,'_mot_analysis_result.txt', sep="")
 	write.table (finOut, file=Mad3ResultOut, sep="\t", quote=F, row.names=F)
+	cat("\n##########\nCompleted!\n##########")
 }
