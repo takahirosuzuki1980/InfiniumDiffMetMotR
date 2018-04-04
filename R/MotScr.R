14b92370662632a832a383f2cbb847f95e00a263
takahirosuzuki1980 <takahirosuzuki1980@gmail.com>
2018-04-04 15:13:33 +0900

    Update MotScr.R

--- R/MotScr.R
+++ R/MotScr.R
@@ -53,7 +53,7 @@ MotScr <- function(infile="sel_processed_Mval.txt", motifDBList = motifDBList, c
 	file.remove(tempDir)
 	gc()
 	
-	cat("Plotting the results...\n\n")
+	cat("Plotting the results...\n")
 	##Plot output setting
 	distPlotFile <- paste(outname,'_plot.pdf', sep="") ##output file name setting
 	pdf(distPlotFile)
@@ -63,7 +63,7 @@ MotScr <- function(infile="sel_processed_Mval.txt", motifDBList = motifDBList, c
 	##Computation by each motif in motifDB
 	for (j in 1:length(target_positionsList)){
 		##monitering of progress (standerd output of vidualizing motif name)
-		indicator_2 <- paste("               ", "MOTIF   ",All_motif_names[j],": ",j, "/", length(All_motif_names),"      ", date(), "\n", sep="") 
+		indicator_2 <- paste("    ", "MOTIF   ",All_motif_names[j],": ",j, "/", length(All_motif_names),"      ", date(), "\n", sep="") 
 		cat(indicator_2)
 
 		motif_name <- All_motif_names[j]
@@ -71,29 +71,29 @@ MotScr <- function(infile="sel_processed_Mval.txt", motifDBList = motifDBList, c
 		ctrl_mot_posi <- unlist(random_positionsList[[j]])
 
 		## histogram plot
-		cat("Step 1 / Plot Histgram (Figure 1)...\n")
+		cat("    Step 1 / Plot Histgram (Figure 1)...\n")
 		motHist(target_mot_posi, ctrl_mot_posi, seq_range=seq_range, motif_name=motif_name)
 		## motif enrichment score plot
 
-		cat("Step 2 / Plot Enrichment Score (Figure 2)...\n")
+		cat("    Step 2 / Plot Enrichment Score (Figure 2)...\n")
 		enrichment_scores <- enrichScoreDist(target_mot_posi, ctrl_mot_posi, seq_range=seq_range, motif_name=motif_name, nDMP_IDs=nDMP_IDs, plot_draw=TRUE)
 
-		cat("Step 3 / Creating a count.pvalue Matrix...\n")
+		cat("    Step 3 / Creating a count.pvalue Matrix...\n")
 		windowSize <- 100
 		motif_counts_matrix <- countMatrix(target_mot_posi = target_mot_posi, ctrl_mot_posi = ctrl_mot_posi, seq_range=seq_range, windowSize = windowSize, slide = 50)
 
 		if((length(target_mot_posi != 0)) && (length(ctrl_mot_posi != 0))){
-			cat("Step 4 / Visualization of FC (Figure 3)...\n")
+			cat("    Step 4 / Visualization of FC (Figure 3)...\n")
 			FCChangePlot(motif_name = motif_name, motif_counts_matrix = motif_counts_matrix)
 
-			cat("Step 5 / Poisson disribution model exact test (Figure 4)...\n")
+			cat("    Step 5 / Poisson disribution model exact test (Figure 4)...\n")
 			PoisTestPlot(motif_counts_matrix = motif_counts_matrix, motif_name = motif_name, seq_range = seq_range)
 		}
 
-		cat("Step 6 / Extraction of significantly enriched ranges...\n")
+		cat("    Step 6 / Extraction of significantly enriched ranges...\n")
 		significant_ranges <- SigRange(motif_counts_matrix = motif_counts_matrix, cutoff = 0.00001, windowSize = windowSize)
 
-		cat("Step 7 / Significance test...\n")
+		cat("    Step 7 / Significance test...\n")
 		parameters <- enrichTest(significant_ranges = significant_ranges, motif_counts_matrix = motif_counts_matrix, seq_range=seq_range, enrichment_scores=enrichment_scores,windowSize=windowSize)
 		parameter_matrix <- rbind(parameter_matrix, parameters)
 	}
@@ -104,5 +104,5 @@ MotScr <- function(infile="sel_processed_Mval.txt", motifDBList = motifDBList, c
 	##out put file name setting
 	Mad3ResultOut <- paste(Sys.Date(),'_',outname,'_mot_analysis_result.txt', sep="")
 	write.table (finOut, file=Mad3ResultOut, sep="\t", quote=F, row.names=F)
-	cat("\n##########\nCompleted!\n##########")
+	cat("Completed!!\n")
 }
