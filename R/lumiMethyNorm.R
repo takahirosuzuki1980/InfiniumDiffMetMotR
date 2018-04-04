789fb6e37a209f2c28315dac33f8ae3bdf11d484
takahirosuzuki1980 <takahirosuzuki1980@gmail.com>
2018-04-04 11:13:00 +0900

    Update lumiMethyNorm.R
    bag fix
    
    minor bags

--- R/lumiMethyNorm.R
+++ R/lumiMethyNorm.R
@@ -8,7 +8,7 @@ lumiMethyNorm <- function(fileName = "TableControl.txt", sample_names = sample_n
 	addAnnotationInfo(data.lumiMethy, lib = 'FDb.InfiniumMethylation.hg19', annotationColumn=c('COLOR_CHANNEL', 'CHROMOSOME', 'POSITION'))
 	sampleNames(data.lumiMethy) <- sample_names	#convert the sampleID to sample name
 	############### 必要なら###################################
-	dir.create("Process_Result")                                   #make a directory to store processing results
+	dir.create("Process_Result")	#make a directory to store processing results
 	##PCA plot
 	pdf("Process_Result/pca.pdf")
 	plotSampleRelation(data.lumiMethy, method='mds', cv.Th=0)
@@ -18,10 +18,10 @@ lumiMethyNorm <- function(fileName = "TableControl.txt", sample_names = sample_n
 	## Check color balance after color balance adjustment
 	pdf("Process_Result/col.adj.pdf")
 	par(mfrow=c(2,2))
-	plotColorBias1D(data.lumiMethy, channel='sum')     #density plot of raw
-	boxplotColorBias(data.lumiMethy, channel='sum')    #boxplot of raw
-	plotColorBias1D(lumiMethy.c.adj, channel='sum')    #density plot of adjusted
-	boxplotColorBias(lumiMethy.c.adj, channel='sum')   #box plot of adjusted
+	plotColorBias1D(data.lumiMethy, channel='sum')	#density plot of raw
+	boxplotColorBias(data.lumiMethy, channel='sum')	#boxplot of raw
+	plotColorBias1D(lumiMethy.c.adj, channel='sum')	#density plot of adjusted
+	boxplotColorBias(lumiMethy.c.adj, channel='sum')	#box plot of adjusted
 	dev.off()
 	############################### Normalization###############################
 	## Perform SSN normalization based on color balance adjusted data
@@ -39,7 +39,7 @@ lumiMethyNorm <- function(fileName = "TableControl.txt", sample_names = sample_n
 	dataMatrix <- exprs(lumiMethy.c.q)
 	write.exprs(lumiMethy.c.q, file='processed_Mval.txt')
 	#################### Remove the probes which is low detection p-value among all sample.##########################
-	presentCount <- detectionCall(data.lumiMethy, Th = 0.01) #Sample number of "Present" of a probe.
+	presentCount <- detectionCall(data.lumiMethy, Th = 0.01)	#Sample number of "Present" of a probe.
 	selDataMatrix <- dataMatrix[presentCount > 0,]
 	write.table(selDataMatrix, file="sel_processed_Mval.txt", sep="\t", quote=F)
 }
