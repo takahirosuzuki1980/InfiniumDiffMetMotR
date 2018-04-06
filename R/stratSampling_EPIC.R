a431973a6de40f2ade3c1925ad6e4e16862834db
takahirosuzuki1980 <takahirosuzuki1980@gmail.com>
2018-04-06 18:58:42 +0900

    Update stratSampling_EPIC.R
    bag fix

--- R/stratSampling_EPIC.R
+++ R/stratSampling_EPIC.R
@@ -10,7 +10,7 @@ stratSampling_EPIC <- function(target_IDs, EPICanno=EPICanno){	#stratified sampl
   nall.TSS.probes <- length(all.TSS.probes) #number of all CGI probes
   nall.CGI.probes <- length(all.CGI.probes) #number of all CGI probes
   nall.shore.probes <- length(all.shore.probes) #number of all shore probes
-  nall.shore.probes <- length(all.shelf.probes) #number of all shelf probes
+  nall.shelf.probes <- length(all.shelf.probes) #number of all shelf probes
   nall.other.probes <- length (all.other.probes)  #number of all non-CGI/non-shore/probes
 
 
@@ -25,15 +25,15 @@ stratSampling_EPIC <- function(target_IDs, EPICanno=EPICanno){	#stratified sampl
   nTSS.probes <- length(TSS.probes) #number of CGI DMPs
   nCGI.probes <- length(CGI.probes)	#number of CGI DMPs
   nshore.probes <- length(shore.probes)	#number of shore DMPs
-  nshore.probes <- length(shelf.probes) #number of shore DMPs
+  nshelf.probes <- length(shelf.probes) #number of shore DMPs
   nnonCGI.probes <- length(other.probes)	#number of non-CGI/non-shore DMPs
 
   ##ramdom sampling from each categolies
   run.TSS.probes <- all.TSS.probes[floor(runif(nTSS.probes, 1, nall.TSS.probes))] #TSS probes
   run.CGI.probes <- all.CGI.probes[floor(runif(nCGI.probes, 1 ,nall.CGI.probes))]	#CGI probes
   run.shore.probes <- all.shore.probes[floor(runif(nshore.probes, 1 ,nall.shore.probes))]	#shore probes
-  run.shore.probes <- all.shore.probes[floor(runif(nshore.probes, 1 ,nall.shore.probes))] #shelf probes
+  run.shelf.probes <- all.shore.probes[floor(runif(nshelf.probes, 1 ,nall.shelf.probes))] #shelf probes
   run.other.probes <- all.other.probes[floor(runif(nnonCGI.probes, 1 ,nall.other.probes))] 	#other probes
-  run.probes <- c(run.TSS.probes, run.CGI.probes, run.shore.probes, run.other.probes)
+  run.probes <- c(run.TSS.probes, run.CGI.probes, run.shore.probes, run.shelf.probes, run.other.probes)
   return (run.probes)
 }
