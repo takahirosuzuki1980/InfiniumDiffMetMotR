1b210a0e1e852e0fedaa9d0214cfeec9e2dbfad1
takahirosuzuki1980 <takahirosuzuki1980@gmail.com>
2018-04-06 17:21:21 +0900

    Update and rename stratSampling_EPIC.R to stratSampling_EPIC_2.R

--- R/stratSampling_EPIC_2.R
+++ R/stratSampling_EPIC_2.R
@@ -1,4 +1,4 @@
-stratSampling_EPIC <- function(target_IDs, allProbe_IDs, EPICanno=EPICanno){	#stratified sampling based on DMPs. categolies are CpG island, CpG shore and non-CGI/non-shore
+stratSampling_EPIC_2 <- function(target_IDs, allProbe_IDs, EPICanno=EPICanno){	#stratified sampling based on DMPs. categolies are CpG island, CpG shore and non-CGI/non-shore
   library("FDb.InfiniumMethylation.hg19")
 
   selEPICanno <- cbind(EPICanno[,c("CHR","MAPINFO","MAPINFO", "IlmnID")],
