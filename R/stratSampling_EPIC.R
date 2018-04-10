38306cb378201711c4889fe24205324a8f093925
takahirosuzuki1980 <takahirosuzuki1980@gmail.com>
2018-04-10 15:41:08 +0900

    Update stratSampling_EPIC.R
    bag fix
    
    categorization of all probes did not work. number of each category of target probes was wrong.

--- R/stratSampling_EPIC.R
+++ R/stratSampling_EPIC.R
@@ -1,10 +1,11 @@
 stratSampling_EPIC <- function(target_IDs, EPICanno=EPICanno){	#stratified sampling based on DMPs. categolies are CpG island, CpG shore and non-CGI/non-shore
  ##Categolization of all probes into the categolies
-  all.TSS.probes <- EPICanno[unique(c(grep("TSS200", EPICanno[,"UCSC_RefGene_Group"]), grep("5'UTR", EPICanno[,"UCSC_RefGene_Group"]))),"IlmnID"]#identification of all TSS probes
-  all.CGI.probes <- EPICanno[grep("Island", EPICanno[!(EPICanno[,"IlmnID"] %in% all.TSS.probes),"Relation_to_UCSC_CpG_Island"]),"IlmnID"]#identification of all CGI probes
-  all.shore.probes <- EPICanno[grep("Shore", EPICanno[!(EPICanno[,"IlmnID"] %in% all.TSS.probes),"Relation_to_UCSC_CpG_Island"]),"IlmnID"] #identification of all shore probes
-  all.shelf.probes <- EPICanno[grep("Shelf", EPICanno[!(EPICanno[,"IlmnID"] %in% all.TSS.probes),"Relation_to_UCSC_CpG_Island"]),"IlmnID"]
-  all.other.probes <- setdiff(setdiff(setdiff(setdiff(EPICanno[,"IlmnID"],  all.TSS.probes), all.CGI.probes), all.shore.probes),all.shelf.probes)  #identification of all non-TSS, non-CGI, non-shore probes
+  all.TSS.probes <- as.vector(EPICanno[unique(c(grep("TSS200", EPICanno[,"UCSC_RefGene_Group"]), grep("5'UTR", EPICanno[,"UCSC_RefGene_Group"]))),"IlmnID"])#identification of all TSS probes
+  all.nonTSS <- EPICanno[!(EPICanno[,"IlmnID"] %in% all.TSS.probes),]
+  all.CGI.probes <- as.vector(all.nonTSS[grep("Island", all.nonTSS[,"Relation_to_UCSC_CpG_Island"]),"IlmnID"])#identification of all CGI probes
+  all.shore.probes <- as.vector(all.nonTSS[grep("Shore", all.nonTSS[,"Relation_to_UCSC_CpG_Island"]),"IlmnID"]) #identification of all shore probes
+  all.shelf.probes <- as.vector(all.nonTSS[grep("Shelf", all.nonTSS[,"Relation_to_UCSC_CpG_Island"]),"IlmnID"]) #identification of all shelf probes
+  all.other.probes <- setdiff(setdiff(setdiff(setdiff(EPICanno[,"IlmnID"],all.TSS.probes), all.CGI.probes), all.shore.probes),all.shelf.probes)  #identification of all non-TSS, non-CGI, non-shore probes
 
   ##number of probes in each categolies
   nall.TSS.probes <- length(all.TSS.probes) #number of all CGI probes
@@ -15,11 +16,11 @@ stratSampling_EPIC <- function(target_IDs, EPICanno=EPICanno){	#stratified sampl
 
 
   ## identification of DMPs in each categolies (TSS, CpG Island, CpG Shore, others)
-  TSS.probes <- target_IDs %in% all.TSS.probes  #identification of TSS DMPs
-  CGI.probes <- target_IDs %in% all.CGI.probes	#identification of CGI DMPs within non-TSS probes
-  shore.probes <- target_IDs %in% all.shore.probes	#identification of shore DMPs within non-TSS probes
-  shelf.probes <- target_IDs %in% all.shelf.probes #
-  other.probes <- target_IDs %in% all.other.probes  #identification of non-TSS, non-CGI, non-shore probes
+  TSS.probes <- target_IDs[target_IDs %in% all.TSS.probes]  #identification of TSS DMPs
+  CGI.probes <- target_IDs[target_IDs %in% all.CGI.probes]	#identification of CGI DMPs within non-TSS probes
+  shore.probes <- target_IDs[target_IDs %in% all.shore.probes]	#identification of shore DMPs within non-TSS probes
+  shelf.probes <- target_IDs[target_IDs %in% all.shelf.probes] #identification of shelf DMPs within non-TSS probes
+  other.probes <- target_IDs[target_IDs %in% all.other.probes]  #identification of non-TSS, non-CGI, non-shore probes
 
   ## number of DMPs in each categilies
   nTSS.probes <- length(TSS.probes) #number of CGI DMPs
@@ -37,3 +38,4 @@ stratSampling_EPIC <- function(target_IDs, EPICanno=EPICanno){	#stratified sampl
   run.probes <- c(run.TSS.probes, run.CGI.probes, run.shore.probes, run.shelf.probes, run.other.probes)
   return (run.probes)
 }
+
