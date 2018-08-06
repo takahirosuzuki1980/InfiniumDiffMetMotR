f624813889be9e33989af1c03487088db46d6522
takahirosuzuki1980 <takahirosuzuki1980@gmail.com>
2018-08-07 08:57:32 +0900

    bagfix: duplicated probes available

--- R/probeID2position_EPIC.R
+++ R/probeID2position_EPIC.R
@@ -1,9 +1,10 @@
 probeID2position_EPIC <- function (probe_IDs, EPICanno = EPICanno){ #conversion from illumina humanEPIC methylation array ID to genomic position (R version 3.2 or later)
   ##conversion of target probe IDs to positions
   probes <- probe_IDs
-  CHR37 <- as.vector (EPICanno[which(EPICanno[,1]%in% probes), "CHR"])
+  probes_anno <- EPICanno[(EPICanno[,1] %in% probes), ]
+  CHR37 <- sapply(probes, function(x){probes_anno[probes_anno[,1] %in% x, "CHR"]})
   CHR37 <- paste("chr", CHR37, sep="")
-  CPG37 <- as.vector (EPICanno[which(EPICanno[,1] %in% probes), "MAPINFO"])
+  CPG37 <- sapply(probes, function(x){probes_anno[probes_anno[,1] %in% x, "MAPINFO"]})
   positions <- cbind(CHR37, CPG37)
   rownames(positions) <- probes
   return(positions)
