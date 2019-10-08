3bc7d625a5a94e953811f74e61f6df9c90f9acd2
takahirosuzuki1980 <takahirosuzuki1980@gmail.com>
2019-10-08 10:36:54 +0900

    fix reexport

--- R/utilities.R
+++ R/utilities.R
@@ -1,21 +1,23 @@
-#' Utilities
+#' Reexports
 #' 
-#' reload of functions
+#' reexport of functions
 #' 
-#' @name utilities
+#' @name Reexports
 #' 
-#' @import IlluminaHumanMethylationEPICanno.ilm10b2.hg19
 #' @importFrom methylumi betas methylated unmethylated pval.detect<-
 #' @importFrom minfi getAnnotationObject
 #' 
 #' @usage NA
+#' 
 #' @export
-
-
 minfi::getAnnotationObject
+#' @export
 methylumi::betas
+#' @export
 methylumi::methylated
+#' @export
 methylumi::unmethylated
+#' @export
 methylumi::"pval.detect<-"
 
 
