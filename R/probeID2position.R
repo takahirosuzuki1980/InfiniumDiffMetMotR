1adfa39bbf60afdedce8f0e72c4ce1b28a19c655
takahirosuzuki1980 <user.email>
2019-12-27 14:53:56 +0900

    debug

--- R/probeID2position.R
+++ R/probeID2position.R
@@ -14,10 +14,10 @@ probeID2position <- function (probe_IDs, anno_info = InfiniumDiffMetMotR::EPICan
   ##conversion of target probe IDs to positions
   probes <- probe_IDs
   probes_anno <- anno_info[(anno_info[,1] %in% probes), ]
-  CHR37 <- probes_anno[probes_anno[,1] %in% probes, "CHR"]
-  CHR37 <- paste("chr", CHR37, sep="")
-  CPG37 <- as.numeric(probes_anno[probes_anno[,1] %in% probes, "MAPINFO"])
+  CHR37 <- probes_anno[, "CHR"]
+  CHR37 <- paste0("chr", CHR37)
+  CPG37 <- as.numeric(probes_anno[, "MAPINFO"])
   positions <- data.frame(CHR37, CPG37)
-  rownames(positions) <- probes
+  rownames(positions) <- probes_anno[, "IlmnID"]
   return(positions)
 }
